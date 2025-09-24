15ed465430b11d0bbb3c1f993a712d5a0ef0010e
03hpride <hpride@mtech.edu>
2025-09-24 10:37:53 -0600

    Update math_ops.c
    Updated math_ops.c to include Taylor Series functions

--- math_ops.c
+++ math_ops.c
@@ -1,32 +1,26 @@
 // --------- FUNCTION IMPLEMENTATIONS ------------
 
-float multiply(float x, float y) 
-{
+float multiply(float x, float y) {
     return x * y;
 }
 
-float add(float x, float y) 
-{
+float add(float x, float y) {
     return x + y;
 }
 
 // --------- FUNCTION IMPLEMENTATIONS ------------
 
-float square(float x) 
-{
+float square(float x) {
     return x * x;
 }
 
-float cube(float x) 
-{
+float cube(float x) {
     return x * x * x;
 }
 
-float pow(float base, int exponent) 
-{
+float ipow(float base, int exponent) {
     float res = 1;
-    for (int i = 0; i < exponent; i++) 
-    {
+    for (int i = 0; i < exponent; i++) {
         res *= base;
     }
     return res;
@@ -34,42 +28,34 @@ float pow(float base, int exponent)
 
 // --------- SIN AND COS IMPLEMENTATIONS ------------
 
-#define PI 3.14159265358979323846
-
-// Factorial helper (returns int! keep for small values only)
-int factorial(int n) 
-{
-    int res = 1;
-    for (int i = 1; i <= n; i++) 
-    {
+// Factorial helper (small n only)
+double factorial(int n) {
+    double res = 1.0;
+    for (int i = 1; i <= n; i++) {
         res *= i;
     }
     return res;
 }
 
 // Taylor series for sin(x) (x in degrees, steps ~10)
-float sin_taylor(float x, int steps) 
-{
+float sin_taylor(float x, int steps) {
     float rad = x * (PI / 180.0f);   // degrees -> radians
     float result = 0.0f;
 
-    for (int n = 0; n < steps; n++) 
-    {
-        float term = (pow(-1, n) * pow(rad, 2 * n + 1)) / factorial(2 * n + 1);
+    for (int n = 0; n < steps; n++) {
+        float term = (ipow(-1, n) * ipow(rad, 2 * n + 1)) / factorial(2 * n + 1);
         result += term;
     }
     return result;
 }
 
 // Taylor series for cos(x) (x in degrees, steps ~10)
-float cos_taylor(float x, int steps) 
-{
+float cos_taylor(float x, int steps) {
     float rad = x * (PI / 180.0f);   // degrees -> radians
     float result = 0.0f;
 
-    for (int n = 0; n < steps; n++) 
-    {
-        float term = (pow(-1, n) * pow(rad, 2 * n)) / factorial(2 * n);
+    for (int n = 0; n < steps; n++) {
+        float term = (ipow(-1, n) * ipow(rad, 2 * n)) / factorial(2 * n);
         result += term;
     }
     return result;
